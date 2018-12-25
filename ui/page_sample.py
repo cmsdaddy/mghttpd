7f43e4ad961489d2f5520284b6e0e374eb9733af
cyf <chenyf1@126.com>
2018-12-25 17:43:57 +0800

    backup

---
+++ ui/page_sample.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import *
+import ui.mg as mg
+
+
+def show_location_info(request):
+    context = {}
+    context['request'] = request
+    context['sample'] = mg.get_sample_yx()
+    temp = context['sample']
+    print(temp)
+    for state in temp:
+        if temp[state] is 0:
+            temp[state] = 'OFF'
+        elif temp[state] is 1:
+            temp[state] = 'ON'
+    return render(request,'sample/loaction.html', context=context)
+
+def show_analog_info(request):
+    context = {}
+    context['request'] = request
+    context['sample'] = mg.get_sample_yc()
+    return render(request,'sample/analog.html',context=context)
