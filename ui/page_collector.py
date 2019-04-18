a409e1bc7e1c9793486decfaef90de2733420cf3
tom <bedreamer@163.com>
2019-04-18 11:29:40 +0800

    添加路径选择功能

--- ui/page_collector.py
+++ ui/page_collector.py
@@ -13,6 +13,8 @@ from django.shortcuts import render
 from django.http import *
 import ui.scada as scada
 import codecs
+import xlrd
+import re
 
 
 profile_path = scada.profile_dir_path + '/collector.json'
@@ -59,36 +61,88 @@ def show_collector_env_page(request):
         "PROJECT_NAME": '"1',
         "TIMESTAMP_FORMAT": '%Y-%m-%d %H:%M%:S.%f',
     }
-    return render(request, "93-采集器控制管理/06-采集器环境变量.html", context=context)
+    return render(request, "93-采集器控制管理/03-采集器环境变量.html", context=context)
 
 
-def show_collector_yaoce_page(request):
+def show_collector_unit_list_page(request):
     context = dict()
-    #return render(request, "93-采集器控制管理/04-采集器遥测采集控制.html", context=context)
-    return render(request, "93-采集器控制管理/07-采集单元提交表单.html", context=context)
+    return render(request, "93-采集器控制管理/02-采集单元列表.html", context=context)
 
 
-def show_collector_yaoxin_page(request):
+def show_define_collector_unit_page(request):
     context = dict()
-    return render(request, "93-采集器控制管理/02-采集器遥信采集控制.html", context=context)
+    return render(request, "93-采集器控制管理/04-采集单元定义表单.html", context=context)
 
 
-def show_collector_yaokong_page(request):
-    context = dict()
-    return render(request, "93-采集器控制管理/03-采集器遥控采集控制.html", context=context)
+def show_all_matched(excel, sheet_name, node_list):
+    try:
+        sheet = excel.sheet_by_name(sheet_name)
+    except:
+        return list()
+
+    r = re.compile(r'\$\(\S+\)')
+
+    if len(node_list) > 0 and r.match(node_list[0]):
+        prefix = node_list.pop(0) + '/'
+    else:
+        prefix = ''
+
+    if len(node_list) == 0:
+        nodes = list()
+        for line, row in enumerate(sheet.get_rows()):
+            if line == 0:
+                continue
+
+            if row[2].value in {'int', 'float', 'string'}:
+                if int(row[1].value) > 1:
+                    nodes.append(prefix + row[0].value + '/[{}]'.format(int(row[1].value)))
+                else:
+                    nodes.append(prefix + row[0].value)
+            else:
+                if int(row[1].value) > 1:
+                    nodes.append(prefix + row[0].value + '/$(?)')
+                else:
+                    nodes.append(prefix + row[0].value)
+    else:
+        try:
+            next_node_list = node_list[1:]
+        except IndexError:
+            next_node_list = list()
+
+        nodes = show_all_matched(excel, node_list[0], next_node_list)
+        return [prefix + node_list[0] + '/' + node for node in nodes]
+
+    return nodes
 
 
-def show_collector_yaotiao_page(request):
-    context = dict()
-    return render(request, "93-采集器控制管理/05-采集器遥调采集控制.html", context=context)
+databus_profile = scada.profile_dir_path + '/' + '实时中心数据 V1.0T1-20181219(对外版).xlsx'
+_excel = xlrd.open_workbook(databus_profile)
+
+
+def send_back_path_matcher(request):
+    node_list = request.GET['q'].rstrip().lstrip().split('/')
+
+    while '' in node_list:
+        node_list.remove('')
+
+    try:
+        excel = _excel
+        sheet_name = '实时数据总集'
+        paths_list = ['/' + p for p in show_all_matched(excel, sheet_name, node_list)]
+    except Exception as e:
+        print(e)
+        paths_list = list()
+
+    return JsonResponse(paths_list, safe=False)
 
 
 collector_url_map = [
     path('', show_general_collector_profile_page),
     path('env/', show_collector_env_page),
-    path('yaoce/', show_collector_yaoce_page),
-    path('yaoxin/', show_collector_yaoxin_page),
-    path('yaokong/', show_collector_yaokong_page),
-    path('yaotiao/', show_collector_yaotiao_page),
+
+    path('define/', show_define_collector_unit_page),
+    path('define/hotpath/', send_back_path_matcher),
+
+    path('list/', show_collector_unit_list_page),
 ]
 urls = (collector_url_map, 'collector', 'collector')
